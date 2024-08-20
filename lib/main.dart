30706a4ec4dc3700c9000d9bbfe487193086239e
Diego Ventura <diegoventurasg@gmail.com>
2024-08-20 19:05:47 -0300

    add Firebase initializeApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_firebase/auth/login_or_register.dart';
-import 'package:flutter_chat_firebase/auth/auth_gate.dart';
+import 'package:flutter_chat_firebase/firebase_options.dart';
 import 'package:flutter_chat_firebase/themes/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -14,7 +19,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const AuthGate(),
+      home: const LoginOrRegister(),
       theme: lightMode,
     );
   }
